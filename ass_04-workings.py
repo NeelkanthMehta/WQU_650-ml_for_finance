50afed80c4ce507fd30c2a09e6d5c0f7dd48d288
NeelkanthMehta <neilstrong@gmail.com>
2018-10-27 12:00:46 +0000

    1st 7 problems solved, 1 to go

--- ass_04-workings.py
+++ ass_04-workings.py
@@ -22,58 +22,47 @@ from skimage import io
 from ipywidgets import interact
 
 """Problem 01"""
-## Loading image
-#im = io.imread('./cartoon.png')
-#plt.imshow(im/np.max(im), interpolation= 'nearest')
-#plt.show()
-#
-#m, n = im.shape[:2]
-#data = im.reshape(m*n, 3)
-#data = np.array(data, dtype= float)
-#data = data.astype('unit8')
-#
-#
-#wcss = []
-#for i in range(1,11):
-#    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-#    kmeans.fit(data)
-#    wcss.append(kmeans.inertia_)
-#
-#
-## Elbow plot revealing No. of optimal clusters:
-#plt.plot(range(1,11),wcss)
-#plt.title('The Elbow method')
-#plt.xlabel('No. of clusters')
-#plt.ylabel('WCSS')
-#plt.grid()
-#plt.xticks(np.arange(1,11, step=1))
-#plt.show()
-#
-## Fitting K-means algorithm using optimal clusters
-#kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
-#y_kmeans = kmeans.fit_predict(data)
-#
-##new_image = y_kmeans.reshape(m, n, 3)
-##new_image = np.asarray(new_image, dtype='unit8')
-##plt.imshow(new_image, interpolation= 'nearest')
-##plt.show()
-#
-#plt.scatter(data[y_kmeans == 0, 0], data[y_kmeans == 0, 1], s=10, c='red', label='cluster 1')
-#plt.scatter(data[y_kmeans == 1, 0], data[y_kmeans == 1, 1], s=10, c='green', label='cluster 2')
-#plt.scatter(data[y_kmeans == 2, 0], data[y_kmeans == 2, 1], s=10, c='blue', label='cluster 3')
-#plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:1], s=30, c='yellow', label='cluster centers')
-#plt.title('K-means cartoon')
-#plt.xlabel('m')
-#plt.ylabel('n')
-#plt.legend(loc='best')
-#plt.show()
+from skimage import io
+
+im = io.imread('./cartoon.png')
+ax = plt.axes(xticks = [], yticks = [])
+ax.imshow(im)
+
+im.shape
+df = im/ np.max(im)
+df = df.reshape(225 * 225, 3)
+df.shape
+
+plot_pixels(df, title="Original Image")
+
+# Resizing
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=3, init='k-means++', random_state=0)
+kmeans.fit(df)
+new_colours = kmeans.cluster_centers_[kmeans.predict(df)]
+
+im_recolored = new_colours.reshape(im.shape)
+
+plt.imshow(im_recolored)
+plt.show()
+
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 6), subplot_kw=dict(xticks=[], yticks=[])) 
+fig.subplots_adjust(wspace=0.05)
+ax[0].imshow(im)
+ax[0].set_title('Original Image', size=16)
+ax[1].imshow(im_recolored)
+ax[1].set_title('16-color Image', size=16)
+
 
 """Problem 02"""
 from sklearn.datasets import load_digits
 
 # Loading data
 digits = load_digits()
-#
+digits.data.shape
+
 ## finding number of unique labels
 #number_digits = len(np.unique(digits.target))
 #
@@ -107,14 +96,14 @@ digits = load_digits()
 
 
 """Problem 03"""
-lda = LDA(n_components=2)
-
-X_train = lda.fit_transform(X_train,digits.target)
+#lda = LDA(n_components=2)
+#
+#X_train = lda.fit_transform(X_train,digits.target)
 
 """Problem 04"""
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test, images_train, images_test = train_test_split(X_train, digits.target, digits.images, test_size=0.25, random_state=42)
+#X_train, X_test, y_train, y_test, images_train, images_test = train_test_split(X_train, digits.target, digits.images, test_size=0.25, random_state=42)
 
 # no. of training features
 n_samples, n_features = X_train.shape
@@ -123,7 +112,7 @@ n_digits = len(np.unique(y_train))
 
 wcss = []
 for i in range(1,11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', n_init=10, max_iter=300, random_state=42)
+    kmeans = KMeans(n_clusters=i, init='k-means++', n_init==10, max_iter=300, random_state=42)
     kmeans.fit_predict(X_train)
     wcss.append(kmeans.inertia_)
 
@@ -154,3 +143,21 @@ plt.show()
 y_kmeans_pred = kmeans.predict(X_test)
 
 C_M = confusion_matrix(y_test, y_kmeans_pred)
+
+
+
+"""Gaussian Mixture Models"""
+from sklearn.mixture import GaussianMixture
+
+GMM = GaussianMixture(n_components=4).fit(X)
+labels = GMM.predict(X)
+
+plt.scatter(X[:,0], X[:,1], c=labels, s=40, cmap='viridis')
+plt.show()
+
+probs = GMM.predict_proba(X)
+
+size = 50 * probs.max(1) ** 2 # Square emphesizes the differences
+plt.scatter(X[:,0], X[:,1], c=labels, cmap='viridis', s=size)
+plt.show()
+
